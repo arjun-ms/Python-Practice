330993b1fb8041729d0651b59f9dabd2cf0cf232
arjun-ms <arjunachu123.aa@gmail.com>
2022-05-27 18:53:52 +0530

    Class Attribute

--- OOP/vehicle.py
+++ OOP/vehicle.py
@@ -1,5 +1,7 @@
 class Vehicle:
-
+    # class attribute
+    color = "White"
+    
     def __init__(self,name,max_speed,milege):
         self.name = name
         self.max_speed = max_speed
@@ -8,16 +10,22 @@ class Vehicle:
     def seating_capacity(self,capacity):
         print(f"The seating capacity of a {self.name} is {capacity} passengers")
 
+
 class Bus(Vehicle):
     def seating_capacity(self,capacity=50):
         print(f"The seating capacity of a {self.name} is {capacity} passengers")
+
+
+
 
 Tesla = Vehicle("Model X",500,50)
 print(f"Max speed: {Tesla.max_speed}")
 print(f"Milege: {Tesla.milege}")
 Tesla.seating_capacity(4)
+print(Tesla.color)
 
 Ksrtc = Bus("TVM",100,25)
 print(f"Max speed: {Ksrtc.max_speed}")
 print(f"Milege: {Ksrtc.milege}")
 Ksrtc.seating_capacity(100)
+print(Ksrtc.color)
